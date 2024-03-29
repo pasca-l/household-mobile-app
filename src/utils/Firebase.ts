cf54e08db041b1b3edc055767b6e59a6a4ecf827
pasca-l <shion.yamadate@keio.jp>
2024-03-30 00:32:14 +0900

    added firestore

--- src/utils/Firebase.ts
+++ src/utils/Firebase.ts
@@ -1,13 +1,12 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
 // import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
 // import {...} from "firebase/functions";
 // import {...} from "firebase/storage";
 
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBP6AehZPKBg8Ugg4C-3JJHmopGR_sMdwQ",
   authDomain: "household-app-921c0.firebaseapp.com",
@@ -17,4 +16,8 @@ const firebaseConfig = {
   appId: "1:443616909343:web:45b1bf3995b41ab73a5d2a",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
