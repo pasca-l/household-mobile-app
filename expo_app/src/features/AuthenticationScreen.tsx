d56217b1c1047778106426f5507883678bb499df
pasca-l <shion.yamadate@keio.jp>
2024-05-04 23:43:14 +0900

    added authentication

---
+++ expo_app/src/features/AuthenticationScreen.tsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { Button, Text, TextInput } from "react-native-paper";
+
+import {
+  browserSessionPersistence,
+  onAuthStateChanged,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { AUTHENTICATION } from "@/utils/firebaseUtils";
+import { GOOGLE_PROVIDER } from "@/utils/authProviders";
+
+export default function AuthenticationScreen() {
+  const [userName, setUserName] = useState("");
+  const [userUID, setUserUID] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(AUTHENTICATION, async (user) => {
+      if (user) {
+        setUserName(String(user.email));
+        setUserUID(String(user.uid));
+      } else {
+        setUserName("undefined");
+        setUserUID("undefined");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <>
+      <Text>{userName}</Text>
+      <Text>{userUID}</Text>
+      <Button
+        onPress={() => {
+          setPersistence(AUTHENTICATION, browserSessionPersistence).then(
+            async () => {
+              await signInWithPopup(AUTHENTICATION, GOOGLE_PROVIDER);
+            }
+          );
+        }}
+      >
+        Sign in with Google
+      </Button>
+      <TextInput
+        mode="flat"
+        label="email"
+        value={inputEmail}
+        onChangeText={setInputEmail}
+        placeholder="abc@gmail.com"
+      />
+      <TextInput
+        mode="flat"
+        label="password"
+        value={inputPassword}
+        onChangeText={setInputPassword}
+        secureTextEntry={true}
+      />
+      <Button
+        onPress={() => {
+          signInWithEmailAndPassword(AUTHENTICATION, inputEmail, inputPassword);
+        }}
+      >
+        Sign in with email and password
+      </Button>
+      <Button
+        onPress={() => {
+          signOut(AUTHENTICATION);
+        }}
+      >
+        Sign out
+      </Button>
+    </>
+  );
+}
--- expo_app/src/features/spendings/SpendingsScreen.tsx
+++ expo_app/src/features/spendings/SpendingsScreen.tsx
@@ -3,11 +3,13 @@ import { Divider, Text } from "react-native-paper";
 
 import SpendingsForm from "./components/SpendingsForm";
 import SpendingsDataTable from "./components/SpendingsDataTable";
+import AuthenticationScreen from "../AuthenticationScreen";
 
 export default function SpendingsScreen() {
   return (
     <View style={styles.container}>
-      <Text variant="displaySmall">Tab One</Text>
+      <Text variant="displaySmall">Spendings</Text>
+      <AuthenticationScreen />
       <Divider />
       <SpendingsForm />
       <Divider />
